7993cd7f3a0505a6f0d92e46462cb8a844a7d800
Ruslan Curbanov <ruslan.curbanov@uni-duesseldorf.de>
2019-03-22 10:16:55 +0100

    add Gradle project skeleton

---
+++ src/main/java/de/hhu/bsinfo/evlp2boel/EvlpToBoelConverter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package de.hhu.bsinfo.evlp2boel;
+
+/**
+ * @author Ruslan Curbanov, ruslan.curbanov@uni-duesseldorf.de, 22.03.2019
+ *
+ */
+public final class EvlpToBoelConverter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
